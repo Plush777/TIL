38b4fa62875705e327df418664fd872877328789
Plush777 <sky11916@naver.com>
2022-08-25 21:29:57 +0900

    :memo: Ep.4 라우터 2 : navigate , nested routes , outlet 추가

--- 리액트_기초부터_쇼핑몰프로젝트까지/Part.2-쇼핑몰/shop/src/App.jsx
+++ 리액트_기초부터_쇼핑몰프로젝트까지/Part.2-쇼핑몰/shop/src/App.jsx
@@ -1,19 +1,23 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Nav, Navbar } from 'react-bootstrap';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import Detail from './components/Detail';
 import Main from './components/Main';
+import About from './components/About';
+import Event from './components/Event';
 
 function App() {
+    let navigate = useNavigate();
+
     return (
         <div className="App">
             <Navbar bg="dark" variant="dark">
                 <Container>
                     <Navbar.Brand href="#home">Navbar</Navbar.Brand>
                     <Nav className="me-auto">
-						<Link to='/'>홈</Link>
-						<Link to='/detail'>상세 페이지</Link>
+                        <Nav.Link onClick={() => { navigate('/') }}>홈</Nav.Link>
+                        <Nav.Link onClick={() => { navigate('/detail') }}>상세 페이지</Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
@@ -21,6 +25,17 @@ function App() {
             <Routes>
                 <Route path="/" element={<Main/>} />
                 <Route path="detail" element={<Detail/>} />
+                {/* 설정한 route 이 외에 경로 입력 시 안돼돌아가 가 출력됨! */}
+                <Route path="*" element={<div>안돼 돌아가</div>} /> 
+                <Route path="/about" element={<About/>}>
+                    <Route path='member' element={<div>멤버임</div>}/>
+                    <Route path='location' element={<div>위치정보임</div>}/>
+                </Route>
+
+                <Route path="/event" element={<Event/>}>
+                    <Route path='one' element={<div>사실 없음 ㅎ</div>}/>
+                    <Route path='two' element={<div>진짜 없음</div>}/>
+                </Route>
             </Routes>
         </div>
     );
