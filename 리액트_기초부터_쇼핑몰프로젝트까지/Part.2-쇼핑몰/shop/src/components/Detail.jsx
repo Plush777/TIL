f0b0fa335a8fc5fc1b67fc2edc98f057a97bf665
Plush777 <sky11916@naver.com>
2022-08-27 22:05:52 +0900

    :memo: Ep.7 , Ep.8 추가

--- 리액트_기초부터_쇼핑몰프로젝트까지/Part.2-쇼핑몰/shop/src/components/Detail.jsx
+++ 리액트_기초부터_쇼핑몰프로젝트까지/Part.2-쇼핑몰/shop/src/components/Detail.jsx
@@ -1,7 +1,9 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from 'styled-components';
 
 const Container = styled.div`
+    position: relative;
     display: flex;
     justify-content: center;
 `
@@ -27,7 +29,7 @@ const ProductsName = styled.h4`
     color: ${props => props.color};
 `
 
-const ProductsButton = styled.button`
+const ProductsButton = styled.button.attrs({type: 'button'})`
     width: 100%;
     height: ${props => props.btnHeight};
     padding: 0 10px;
@@ -42,15 +44,68 @@ const ProductsButton = styled.button`
     }
 `
 
+const Alert = styled.div`
+    position: absolute;
+    top: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 10px;
+    width: 500px;
+    height: 50px;
+    color: #fff;
+    border-radius: 4px;
+    background-color: #000;
+`
+
+const AlertType02 = styled(Alert)`
+    
+`
+
+const Input = styled.input.attrs({type:'text'})`
+    height: 35px;
+    margin-top: ${props => props.mt};
+    border: 1px solid #ddd;
+    background-color: #fff;
+`
+
 function Detail({ shoes }) {
+
+    let [alert,setAlert] = useState(true);
+    let [newAlert,setNewAlert] = useState(false);
+    let [isNum,setIsNum] = useState('');
+
+    useEffect(() => {
+        let timer = setTimeout(() => {
+            setAlert(false);
+        },2000)
+
+        if (isNaN(isNum) === true){
+            setNewAlert(true);
+        }
+        return () => {
+            clearTimeout(timer);
+        }
+    }, [isNum,newAlert]);
+
     const shoesData = shoes;
 
     let {id} = useParams();
     id = parseInt(id); //문자 -> 숫자로 변환
     let searchData = shoesData.find(x => x.id === id);
 
+    const inputCurrent = (e) => {
+        setIsNum(e.target.value);
+    }
+
     return (  
         <Container>
+            {
+                alert === true ? (<Alert>2초이내 구매시 할인</Alert>) : null
+            }
+            {
+                newAlert === true ? (<AlertType02>숫자만 입력해주세요.</AlertType02>) : null
+            }
             <ItemRow>
                 <Item>
                     <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" alt="" />
@@ -60,7 +115,8 @@ function Detail({ shoes }) {
                         <ProductsName color="#000" fz="1.5rem">{searchData.title}</ProductsName>
                         <ProductsName color="#000" fz="1.1rem">{searchData.content}</ProductsName>
                         <ProductsName color="#000" fz="1rem">{searchData.price}</ProductsName>
-                        <ProductsButton btnHeight="38px" btnColor="#fff" btnFz="1rem" btnBackground="#dc3545">주문하기</ProductsButton> 
+                        <ProductsButton btnHeight="38px" btnColor="#fff" btnFz="1rem" btnBackground="#dc3545">주문하기</ProductsButton>
+                        <Input onChange={inputCurrent} mt="10px" placeholder="숫자만 입력하세요"/> 
                     </ProductsInfo>  
                 </Item>
             </ItemRow>
--- 리액트_기초부터_쇼핑몰프로젝트까지/Part.2-쇼핑몰/shop/src/components/GlobalStyle.jsx
+++ 리액트_기초부터_쇼핑몰프로젝트까지/Part.2-쇼핑몰/shop/src/components/GlobalStyle.jsx
@@ -2,7 +2,7 @@ import {createGlobalStyle} from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
     *{box-sizing: border-box;}
-    button{border: 0;}
+    button,input{border: 0;}
 `
 
 export default GlobalStyle;
