2cef97068aa9f8524e74b3bcd2ec6823c86a3002
Plush777 <sky11916@naver.com>
2022-09-06 22:34:24 +0900

    :memo: Ep.19 리액트에서 자주쓰는 if문 5개

--- 리액트_기초부터_쇼핑몰프로젝트까지/Part.2-쇼핑몰/shop/src/store.js
+++ 리액트_기초부터_쇼핑몰프로젝트까지/Part.2-쇼핑몰/shop/src/store.js
@@ -17,18 +17,21 @@ let cartData = createSlice({
     },
   ],
 
-  reducers: { //함수 생성
+  reducers: { //state 변경 함수 생성
     modifyCartData(state,action){
       let num = state.findIndex(item => {return item.id === action.payload})
       state[num].count++;
     },
     addItem(state,action){
       state.push(action.payload)
+      //payload => 함수 파라미터 출력문법
     }
   }
 })
 
+//만든 함수 export
 export let {modifyCartData , addItem} = cartData.actions
+// cardData.actions => state 변경함수를 남음
 
 export default configureStore({
   reducer: { //state 만든거 등록
